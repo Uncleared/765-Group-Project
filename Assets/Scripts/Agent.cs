2c7639da00b280bd47478e7c43939dbbb9faf807
Yuewei Zhang <yzhb544@aucklanduni.ac.nz>
2023-05-28 19:10:04 +1200

    Worked on the food distance sensor

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
-    public LayerMask layerMask;
+    public LayerMask foodLayerMask;
+    public LayerMask agentLayerMask;
     public float foodDetectRadius = 3f;
 
     public float foodDot = 0f;
@@ -23,6 +24,39 @@ public class Agent : MonoBehaviour
     }
 
     public Vector3 DetectFoodDirection()
+    {
+        Vector3 foodDirection = Vector3.zero;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, this.foodDetectRadius, foodLayerMask);
+
+        List<Vector3> displacements = new List<Vector3>();
+        foreach (var hitCollider in hitColliders)
+        {
+            // Ignore itself
+            if (hitCollider.gameObject != gameObject)
+            {
+                // Do a dot product check
+                Vector3 delta = hitCollider.transform.position - transform.position;
+                if (Vector3.Dot(delta.normalized, transform.forward) > 0f)
+                {
+                    displacements.Add(delta);
+                }
+            }
+        }
+        displacements.Sort((d1, d2) => d1.magnitude.CompareTo(d2.magnitude));
+        if(displacements.Count > 0)
+        {
+            foodDirection = displacements[0];
+        }
+
+        foreach(Vector3 d in displacements)
+        {
+            print(d.magnitude);
+        }
+        return foodDirection;
+    }
+
+
+    public Vector3 DetectAgentDirection(LayerMask layerMask)
     {
         Vector3 foodDirection = Vector3.zero;
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, this.foodDetectRadius, layerMask);
@@ -54,13 +88,14 @@ public class Agent : MonoBehaviour
         return foodDirection;
     }
 
-    public float DetectFoodValue()
+    public float DetectFoodValue(LayerMask layerMask)
     {
         currentFoodDirection = DetectFoodDirection();
         float dot = Vector3.Dot(transform.right, currentFoodDirection.normalized);
 
         return dot/2f + 0.5f;
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -73,7 +108,12 @@ public class Agent : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.F))
         {
-            foodDot = DetectFoodValue();
+            foodDot = DetectFoodValue(foodLayerMask);
         }
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    foodDot = DetectFoodValue(agentLayerMask);
+        //}
     }
 }
