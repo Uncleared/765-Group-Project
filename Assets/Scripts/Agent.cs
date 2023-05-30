90b5e37766dc76e49ab013bde5780cbb1593d4bc
Uncleared <blackjack27304@gmail.com>
2023-05-31 01:09:48 +1200

    The agent now seeks food

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
+    public float speed = 3f;
+    public float bounds = 30f;
+    public float eatMagnitude = 0.5f;
     public LayerMask foodLayerMask;
     public LayerMask agentLayerMask;
     public float foodDetectRadius = 3f;
 
     public float foodDot = 0f;
+
+    Vector3 velocity;
     public void DetectDirection()
     {
 
@@ -36,6 +41,11 @@ public class Agent : MonoBehaviour
             {
                 // Do a dot product check
                 Vector3 delta = hitCollider.transform.position - transform.position;
+
+                if(delta.magnitude < eatMagnitude)
+                {
+                    Destroy(hitCollider.gameObject);
+                }
                 if (Vector3.Dot(delta.normalized, transform.forward) > 0f)
                 {
                     displacements.Add(delta);
@@ -52,14 +62,14 @@ public class Agent : MonoBehaviour
         {
             print(d.magnitude);
         }
-        return foodDirection;
+        return foodDirection.normalized;
     }
 
 
-    public Vector3 DetectAgentDirection(LayerMask layerMask)
+    public Vector3 DetectAgentDirection()
     {
         Vector3 foodDirection = Vector3.zero;
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, this.foodDetectRadius, layerMask);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, this.foodDetectRadius, agentLayerMask);
 
         List<Vector3> validDirections = new List<Vector3>();
         foreach (var hitCollider in hitColliders)
@@ -85,7 +95,7 @@ public class Agent : MonoBehaviour
             }
             foodDirection /= validDirections.Count;
         }
-        return foodDirection;
+        return foodDirection.normalized;
     }
 
     public float DetectFoodValue()
@@ -98,7 +108,7 @@ public class Agent : MonoBehaviour
 
     public float DetectAgentValue(LayerMask layerMask)
     {
-        currentFoodDirection = DetectAgentDirection(layerMask);
+        currentFoodDirection = DetectAgentDirection();
         float dot = Vector3.Dot(transform.right, currentFoodDirection.normalized);
 
         return dot / 2f + 0.5f;
@@ -107,12 +117,20 @@ public class Agent : MonoBehaviour
 
     public void SeekFood()
     {
-
+        Vector3 foodVelocity = DetectFoodDirection();
+        if(foodVelocity.magnitude > 0.05f)
+        {
+            velocity = foodVelocity;
+        }
     }
 
     public void SeekAgent()
     {
+        Vector3 agentVelocity = DetectAgentDirection();
+        if(velocity.magnitude > 0.05f)
+        {
 
+        }
     }
 
 
@@ -134,5 +152,40 @@ public class Agent : MonoBehaviour
         {
             foodDot = DetectAgentValue(agentLayerMask);
         }
+
+        bool reachedXBound = transform.position.x < -bounds || transform.position.x > bounds;
+        bool reachedZBound = transform.position.z < -bounds || transform.position.z > bounds;
+        // Check bounds
+        if (reachedXBound)
+        {
+            if(transform.position.x < -bounds)
+            {
+                transform.position = new Vector3(bounds, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(-bounds, transform.position.y, transform.position.z);
+            }
+        }
+
+        if (reachedZBound)
+        {
+            if (transform.position.z < -bounds)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, bounds);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, -bounds);
+            }
+        }
+        SeekFood();
+
+        if(velocity.magnitude > 0.03f)
+        {
+            transform.rotation = Quaternion.LookRotation(velocity);
+        }
+  
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
